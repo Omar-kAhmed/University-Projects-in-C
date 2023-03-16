1e0f72b2fcb4992431d9cc8bca029b0a9e4836bf
Omar K. Ahmed <111398535+Omar-kAhmed@users.noreply.github.com>
2023-03-16 10:29:30 +0000

    omar

---
+++ Gcd/gcd.c/#include<stdio.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib>
+#include<time.h>
+
+
+
+void fill_array( int *numbers, int length, int main, int max){
+
+
+
+}for (int i=0; i<length;i++){
+
+    numbers[i]= rand() % (max - min) + min;
+
+}
+
+
+void print_array(int *numbers, int lenght){
+
+printf("%d",numbers[0])
+}for (int i=0; i<length;i++){
+
+printf("%d",numbers[i])
+}
+
+int main(){
+
+srand(time(NULL);
+
+int number[10]
+fill_array(numbers,sizeof(numbers), sizeof(int),1,10);
+
+    return 0;
+}
